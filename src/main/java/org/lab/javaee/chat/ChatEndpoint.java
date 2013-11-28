513134f7bc1b0595bfa1efaaec705401298d51b1
Antoine Sabot-Durand <antoine@sabot-durand.net>
2013-11-28 13:12:50 +0100

    Correcting merge from branch 4th

--- src/main/java/org/lab/javaee/chat/ChatEndpoint.java
+++ src/main/java/org/lab/javaee/chat/ChatEndpoint.java
@@ -39,11 +39,16 @@
  */
 package org.lab.javaee.chat;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.stream.JsonParsingException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.StringReader;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -70,8 +75,22 @@ public class ChatEndpoint {
     @PoodleFilter
     @MappleFilter
     public void message(String message, Session client) {
+        System.out.println(message);
+        JsonReader reader = Json.createReader(new StringReader(message));
+        try {
+            JsonObject msgObj = reader.readObject();
+            Message msg = new Message();
+            msg.setUser(msgObj.getString("user"));
+            msg.setContent(msgObj.getString("content"));
+            System.out.println("Message from " + msg.getUser() + " : " + msg.getContent());
+        } catch (JsonParsingException e) {
+            System.out.println("Message is not in JSON format");
+        } finally {
+            reader.close();
+        }
         for (Session peer : peers) {
             peer.getAsyncRemote().sendText(message);
         }
+
     }
 }
